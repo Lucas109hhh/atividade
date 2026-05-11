6a55c9b357defd93502e8e1e27b4101853e63323
LucassMotos <ltshqr.p@gmail.com>
2026-05-11 14:59:26 -0300

    Aula JS - Introdução a sintaxe

---
+++ ex01.js
@@ -0,0 +1,7 @@
+const input = require ("readline-sync")
+
+let nome = input.question("Qual seu nome?")
+let idade = input.questionInt("Qual sua idade?")
+let cidade = input.question("Qual sua cidade?")
+
+console.log(`Olá, meu nome é ${nome}, eu tenho ${idade}, e vivo em ${cidade}`)
